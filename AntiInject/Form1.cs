598adee3bb7f9465913d0083ccd2d9204a27f5e9
Vladimir <71991079+Vladimir-0001@users.noreply.github.com>
2022-06-08 10:26:32 -0400

    main

---
+++ AntiInject/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+
+namespace AntiInject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            watcher w = new watcher();
+            {
+                
+                
+                Thread watcher = new Thread(w.Watch);
+                watcher.Start();
+            }
+            this.Hide();
+            this.ShowInTaskbar = false;
+        }
+    }
+}
